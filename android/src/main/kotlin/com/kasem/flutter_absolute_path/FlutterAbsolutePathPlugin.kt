b26ec50954980921e715e71e95081519db06d9d1
vvk-home <frostyland@gmail.com>
2021-09-14 11:17:56 +0500

    code: test2 is succes???

--- android/src/main/kotlin/com/kasem/flutter_absolute_path/FlutterAbsolutePathPlugin.kt
+++ android/src/main/kotlin/com/kasem/flutter_absolute_path/FlutterAbsolutePathPlugin.kt
@@ -1,28 +1,24 @@
 package com.kasem.flutter_absolute_path
 
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
-import android.os.Environment
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
-import android.util.Log
-import android.content.pm.ProviderInfo
-import android.content.pm.PackageManager
-import android.content.pm.PackageInfo
-import java.security.Provider
 
 
-class FlutterAbsolutePathPlugin(private val context: Context) : MethodCallHandler {
+class FlutterAbsolutePathPlugin : MethodCallHandler, FlutterPlugin {
+
+    private lateinit var context: Context
 
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "flutter_absolute_path")
-            channel.setMethodCallHandler(FlutterAbsolutePathPlugin(registrar.context()))
+            channel.setMethodCallHandler(FlutterAbsolutePathPlugin())
         }
     }
 
@@ -43,6 +39,14 @@ class FlutterAbsolutePathPlugin(private val context: Context) : MethodCallHandle
             }
             else -> result.notImplemented()
         }
+    }
+
+    override fun onAttachedToEngine(p0: FlutterPlugin.FlutterPluginBinding) {
+        context = p0.applicationContext
+    }
+
+    override fun onDetachedFromEngine(p0: FlutterPlugin.FlutterPluginBinding) {
+        TODO("Not yet implemented")
     }
 
 //    val applicationProviders: List<ProviderInfo>? by lazy {
